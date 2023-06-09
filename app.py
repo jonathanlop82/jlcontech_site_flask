41d5bad9669fa795d13877fc80ebf326090e04c3
Jonathan Lopez <jlopez@Jonathans-MacBook-Pro.local>
2023-06-09 18:03:51 -0500

    Update bugs 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from sendmail import send_mail
 from sendtelegram import send_telegram_message
 
@@ -6,12 +6,9 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/')
+@app.route('/', methods=("POST","GET"))
 def hello_world():  # put application's code here
-    return render_template('index.html')
 
-@app.route('/form', methods=("POST","GET"))
-def form():
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
@@ -25,10 +22,17 @@ def form():
         send_mail("Nuevo Prospecto desde Formulario Web", "ventas@jlcontech.com", "ventas@jlcontech.com", name, email, subject, company, message)
         mensaje_telegram = f'Nuevo Prospecto: Nombre: {name}, Email: {email}, Asunto: {subject}, Empresa: {company}, Mensaje: {message}'
         send_telegram_message(mensaje_telegram)
+        return render_template('index.html')
 
-    return render_template("index.html")
+    return render_template('index.html')
+
+#@app.route('/form', methods=("POST","GET"))
+#def form():
+
+
+#    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
 
